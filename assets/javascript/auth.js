62829f4247b1b8b709125db1b5083597f84fc028
Derrick <dderrickmatheww@gmail.com>
2019-04-21 22:29:56 -0400

    update on accounts

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -36,10 +36,10 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
     signInFlow: 'popup',
     signInSuccessUrl: 'index.html',
     signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
       // Leave the lines as is for the providers you want to offer your users.
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+     firebase.auth.FacebookAuthProvider.PROVIDER_ID
     //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
     //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
@@ -62,6 +62,7 @@ ui.start('#firebaseui-auth-container', uiConfig);
       $("#login").show()
       $("#login")
     }
+    console.log(user)
   })
   
   //logout
